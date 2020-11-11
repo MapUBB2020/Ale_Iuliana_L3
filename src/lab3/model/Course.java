570b2237a7ed66dc09301a74d74d8c4b9d0aea99
Alexandra Negru <negru_alexandra90@yahoo.com>
2020-11-11 20:58:49 +0200

    Add Course class

---
+++ src/lab3/model/Course.java
@@ -0,0 +1,11 @@
+package lab3.model;
+
+import java.util.List;
+
+public class Course {
+    private String name;
+    private Person teacher;
+    private int maxEnrollment;
+    private List<Student> studentsEnrolled;
+    private int credits;
+}
