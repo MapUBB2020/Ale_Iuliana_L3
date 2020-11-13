2ee8761c6829e84c0250a4781ed8ac8b8a043613
Alexandra Negru <negru_alexandra90@yahoo.com>
2020-11-13 19:23:22 +0200

    Small change

--- src/lab3/model/Course.java
+++ src/lab3/model/Course.java
@@ -3,14 +3,14 @@ package lab3.model;
 import java.util.List;
 
 public class Course {
-    private int id;
+    private Long id;
     private String name;
     private Person teacher;
     private int maxEnrollment;
     private List<Student> studentsEnrolled;
     private int credits;
 
-    public Course(int id, String name, Person teacher, int maxEnrollment, List<Student> studentsEnrolled, int credits) {
+    public Course(Long id, String name, Person teacher, int maxEnrollment, List<Student> studentsEnrolled, int credits) {
         this.id = id;
         this.name = name;
         this.teacher = teacher;
@@ -19,11 +19,11 @@ public class Course {
         this.credits = credits;
     }
 
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
