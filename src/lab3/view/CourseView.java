e4b1dcbb3c5d7d67d08fb19eb42604ef4afe289f
Alexandra Negru <negru_alexandra90@yahoo.com>
2020-11-14 17:02:35 +0200

    Finish View for Course

--- src/lab3/view/CourseView.java
+++ src/lab3/view/CourseView.java
@@ -1,4 +1,17 @@
 package lab3.view;
 
+import lab3.model.Person;
+import lab3.model.Student;
+
+import java.util.List;
+
 public class CourseView {
+    public void printCourseDetails(String courseName, Person courseTeacher, int courseMaxEnrollment, List<Student> courseStudentsEnrolled, int courseCredits) {
+        System.out.println("Course: ");
+        System.out.println("Name: " + courseName);
+        System.out.println("Teacher: " + courseTeacher);
+        System.out.println("Max number of students: " + courseMaxEnrollment);
+        System.out.println("Students enrolled: " + courseStudentsEnrolled);
+        System.out.println("Credits: " + courseCredits);
+    }
 }
