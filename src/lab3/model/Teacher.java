d9aeaf4d0f3c8b502f1d4b8ab80bcd392de685a2
iuliana02 <iuliana02@users.noreply.github.com>
2020-11-14 16:54:23 +0200

    Changed to specialized class of superclass Person

--- src/lab3/model/Teacher.java
+++ src/lab3/model/Teacher.java
@@ -2,11 +2,23 @@ package lab3.model;
 
 import java.util.List;
 
-public class Teacher implements Person{
+public class Teacher extends Person{
+
+    private long teacherID;
     private List<Course> courses;
 
-    public Teacher(List<Course> courses) {
+    public Teacher(List<Course> courses, long teacherID, String firstName, String lastName) {
+        super(firstName,lastName);
         this.courses = courses;
+        this.teacherID = teacherID;
+    }
+
+    public long getTeacherID() {
+        return teacherID;
+    }
+
+    public void setTeacherID(long teacherID) {
+        this.teacherID = teacherID;
     }
 
     public List<Course> getCourses() {
