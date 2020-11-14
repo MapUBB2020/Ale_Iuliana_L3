186182af59a37a13486c3fca2cd88f9847b735cf
iuliana02 <iuliana02@users.noreply.github.com>
2020-11-14 16:56:24 +0200

    Modified to class which extends Person

--- src/lab3/model/Student.java
+++ src/lab3/model/Student.java
@@ -2,18 +2,16 @@ package lab3.model;
 
 import java.util.List;
 
-public class Student implements Person{
+public class Student extends Person{
     private long studentId;
     private int totalCredits;
-    private static List<Course> enrolledCourses;
-    private static List<Student> students;
+    private List<Course> enrolledCourses;
 
-
-    public Student(long studentId, int totalCredits, List<Course> enrolledCourses, List<Student> students) {
+    public Student(long studentId, int totalCredits, List<Course> enrolledCourses, List<Student> students, String firstName, String lastName) {
+        super(firstName,lastName);
         this.studentId = studentId;
         this.totalCredits = totalCredits;
         this.enrolledCourses = enrolledCourses;
-        this.students = students;
     }
 
     public long getStudentId() {
@@ -39,19 +37,5 @@ public class Student implements Person{
         this.enrolledCourses = enrolledCourses;
     }
 
-    public static List<Student> getStudents() {
-        return students;
-    }
-    public void setStudents(List<Student> students) {
-        this.students = students;
-    }
 
-    @Override
-    public String toString(){
-        String string = "";
-        string = this.studentId + " " + this.totalCredits;
-        for (Course course : enrolledCourses)
-            string += course.getName() + ", ";
-        return string;
-    }
 }
