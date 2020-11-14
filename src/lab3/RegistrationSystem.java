c0c58a6196b456fec6487106e81bd3edaa4c7b69
Alexandra Negru <negru_alexandra90@yahoo.com>
2020-11-14 18:12:35 +0200

    Add Registration System

--- src/lab3/RegistrationSystem.java
+++ src/lab3/RegistrationSystem.java
@@ -1,2 +1,22 @@
-package lab3;public class RegistrationSystem {
+package lab3;
+
+import lab3.model.Course;
+import lab3.model.Student;
+
+import java.util.List;
+
+public class RegistrationSystem {
+    public void register(Course course, Student student) {
+        if (course.getMaxEnrollment() == course.getStudentsEnrolled().size()) {
+            List<Course> enrolledCourses = student.getEnrolledCourses();
+            enrolledCourses.add(course);
+            student.setEnrolledCourses(enrolledCourses);
+
+            int credits = student.getTotalCredits();
+            credits += course.getCredits();
+            student.setTotalCredits(credits);
+        } else {
+            System.out.println("Maximum number of students reached! Please choose another course.");
+        }
+    }
 }
