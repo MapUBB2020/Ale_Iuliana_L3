b3ee1ad2d17b36f6e77e457762faa0522eee3d58
Alexandra Negru <negru_alexandra90@yahoo.com>
2020-11-14 16:42:20 +0200

    Finish Course repo

--- src/lab3/repository/CourseRepository.java
+++ src/lab3/repository/CourseRepository.java
@@ -1,2 +1,62 @@
-package lab3.repository;public class CourseRepository {
+package lab3.repository;
+
+import lab3.model.Course;
+import lab3.model.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CourseRepository implements ICrudRepository<Course>{
+    List<Course> courseRepo = new ArrayList<Course>();
+
+    @Override
+    public Course findOne(Long id) {
+        for(Course course: courseRepo)
+            if (course.getId().equals(id))
+              return course;
+        return null;
+    }
+
+    @Override
+    public List<Course> findAll() {
+        return courseRepo;
+    }
+
+    @Override
+    public Course save(Course entity) {
+        if (entity != null){
+            if(findOne(entity.getId()) != null) {
+                return entity;
+            } else {
+                courseRepo.add(entity);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Course delete(Long id) {
+        if (id != null){
+            Course entity = findOne(id);
+            if (entity != null)
+                courseRepo.remove(entity);
+        }
+        return null;
+    }
+
+    @Override
+    public Course update(Course entity) {
+        if (entity != null) {
+            if(findOne(entity.getId()) != null) {
+                entity.setName(entity.getName());
+                entity.setTeacher(entity.getTeacher());
+                entity.setMaxEnrollment(entity.getMaxEnrollment());
+                entity.setStudentsEnrolled(entity.getStudentsEnrolled());
+                entity.setCredits(entity.getCredits());
+            } else {
+                return entity;
+            }
+        }
+        return null;
+    }
 }
