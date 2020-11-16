48b5004ec6250ed1491fb14b2e07578438e34008
Alexandra Negru <negru_alexandra90@yahoo.com>
2020-11-16 22:04:39 +0200

    Print teacher first and last name

--- src/lab3/view/CourseView.java
+++ src/lab3/view/CourseView.java
@@ -9,7 +9,7 @@ public class CourseView {
     public void printCourseDetails(String courseName, Person courseTeacher, int courseMaxEnrollment, List<Student> courseStudentsEnrolled, int courseCredits) {
         System.out.println("Course: ");
         System.out.println("Name: " + courseName);
-        System.out.println("Teacher: " + courseTeacher);
+        System.out.println("Teacher: " + courseTeacher.firstName + " " + courseTeacher.lastName);
         System.out.println("Max number of students: " + courseMaxEnrollment);
         System.out.println("Students enrolled: " + courseStudentsEnrolled);
         System.out.println("Credits: " + courseCredits);
