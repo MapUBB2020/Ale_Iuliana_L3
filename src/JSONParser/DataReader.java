a1b1075b7dfa533fc871fad8c23214e4639672b9
Alexandra Negru <negru_alexandra90@yahoo.com>
2020-11-20 19:32:51 +0200

    Delete interface

--- src/JSONParser/DataReader.java
+++
@@ -1,9 +0,0 @@
-package JSONParser;
-
-import java.io.IOException;
-import java.text.ParseException;
-import java.util.List;
-
-public interface DataReader<T> {
-    public List<T> initialiseData() throws IOException, ParseException;
-}
