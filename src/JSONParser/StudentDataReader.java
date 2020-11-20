2d2af3fc79ca62e535aa1fbc4f09acd543ba1f54
iuliana02 <iuliana02@users.noreply.github.com>
2020-11-20 19:31:20 +0200

    Deleted the interface method

--- src/JSONParser/StudentDataReader.java
+++ src/JSONParser/StudentDataReader.java
@@ -2,19 +2,33 @@ package JSONParser;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import lab3.repository.StudentRepository;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import lab3.model.Student;
 
-public class StudentDataReader<Student> implements DataReader {
-    @Override
-    public List<Student> initialiseData() {
+public class StudentDataReader{
+    public List<Student> initialiseData() throws IOException {
         StudentRepository studentRepository = new StudentRepository();
-        List<Student> repo = (List<Student>) studentRepository.students;
+
+        Path path = Paths.get("students.json");
+        List<Student> studentsFromJson = new ArrayList<Student>();
 
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        gson.toJson(repo);
 
-        return repo;
+        try (Reader reader = Files.newBufferedReader(path)) {
+            studentsFromJson = gson.fromJson(reader,
+                    new TypeToken<List<Student>>(){}.getType());
+            studentsFromJson.forEach(System.out::println);
+        }
+
+        return studentsFromJson;
     }
 }
