2ed01cdc09806cd133abd8aa959d15ed75d16c90
Alexandra Negru <negru_alexandra90@yahoo.com>
2020-11-20 19:37:51 +0200

    Small changes

--- src/JSONParser/CourseDataReader.java
+++ src/JSONParser/CourseDataReader.java
@@ -2,20 +2,30 @@ package JSONParser;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import lab3.model.Course;
 import lab3.repository.CourseRepository;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.List;
 
-public class CourseDataReader<Course> implements DataReader {
+public class CourseDataReader {
 
-    @Override
-    public List<Course> initialiseData() {
-        CourseRepository courseRepository = new CourseRepository();
-        List<Course> repo = (List<Course>) courseRepository.courseRepo;
-
+    public List<Course> initialiseData() throws IOException, ParseException {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        gson.toJson(repo);
 
-        return repo;
+        Path path = Paths.get("courses.json");
+        List<Course> coursesFromJson;
+        try (Reader reader = Files.newBufferedReader(path)) {
+            coursesFromJson = gson.fromJson(reader,
+                    new TypeToken<List<Course>>(){}.getType());
+        }
+        return coursesFromJson;
     }
 }
