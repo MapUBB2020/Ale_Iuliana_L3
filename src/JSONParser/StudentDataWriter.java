4120e0111c2a154a210ef87c7d05f6c34a580787
Alexandra Negru <negru_alexandra90@yahoo.com>
2020-11-22 16:59:50 +0200

    Data writer

--- src/JSONParser/StudentDataWriter.java
+++ src/JSONParser/StudentDataWriter.java
@@ -8,21 +8,22 @@ import lab3.repository.StudentRepository;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
 public class StudentDataWriter {
-    public void writeData() throws IOException {
-        StudentRepository studentRepository = new StudentRepository();
+    public void writeData(List<Student> students) throws IOException {
 
+        students.forEach(System.out::println);
         Path path = Paths.get("students.json");
-        try (Writer writer = Files.newBufferedWriter(path)) {
+        try (Writer writer = Files.newBufferedWriter(path,  StandardCharsets.UTF_8)) {
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
-            gson.toJson(studentRepository.students, writer);
+            gson.toJson(students, writer);
         }
-        System.out.println(studentRepository.students.get(0));
+        System.out.println(students.get(0));
 
 
     }
