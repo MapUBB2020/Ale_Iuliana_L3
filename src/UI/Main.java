fc818e66dcafe8fb834770bd1b31847c79aa1276
Alexandra Negru <negru_alexandra90@yahoo.com>
2020-12-10 13:56:41 +0200

    Create main for Javafx app

---
+++ src/UI/Main.java
@@ -0,0 +1,25 @@
+package UI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    static Stage primaryStage;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Auto Questionnaire");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+}
