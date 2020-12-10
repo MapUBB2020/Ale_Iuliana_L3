eb17d460a5664c52ff7bd299230ca3eb431626c6
iuliana02 <iuliana02@users.noreply.github.com>
2020-12-10 19:05:56 +0200

    Commented all

--- src/lab3/StartApp.java
+++ src/lab3/StartApp.java
@@ -1,50 +1,50 @@
-package lab3;
-
-import Exceptions.IncorrectFileNameException;
-import JSONParser.CourseDataReader;
-import JSONParser.StudentDataWriter;
-import JSONParser.TeacherDataReader;
-import lab3.model.Teacher;
-import lab3.repository.CourseRepository;
-import lab3.repository.StudentRepository;
-import lab3.repository.TeacherRepository;
-
-import java.io.IOException;
-import java.text.ParseException;
-
-/**
- * Main class where program starts.
- */
-public class StartApp {
-
-    /**
-     * Start point of the application
-     * @param args command line arguments
-     */
-    public static void main(String[] args) throws IOException, ParseException, NullPointerException, IncorrectFileNameException {
-        TeacherRepository teacherRepository = new TeacherRepository();
-
-        teacherRepository.initialise();
-
-        CourseRepository courseRepository = new CourseRepository(teacherRepository.teachers);
-        courseRepository.initialise();
-
-        StudentRepository studentRepository = new StudentRepository(courseRepository.courseRepo);
-        try {
-            studentRepository.initialise();
-        } catch (Exception NullPointerException) {
-            System.out.println("No students");
-        }
-
-        courseRepository.setRelations(studentRepository);
-        teacherRepository.setRelations(courseRepository);
-
-        RegistrationSystem registrationSystem = new RegistrationSystem(courseRepository,
-                studentRepository,
-                teacherRepository);
-
-        Console console = new Console(registrationSystem);
-        console.run();
-        registrationSystem.writeToJson();
-    }
-}
+//package lab3;
+//
+//import Exceptions.IncorrectFileNameException;
+//import JSONParser.CourseDataReader;
+//import JSONParser.StudentDataWriter;
+//import JSONParser.TeacherDataReader;
+//import lab3.model.Teacher;
+//import lab3.repository.CourseRepository;
+//import lab3.repository.StudentRepository;
+//import lab3.repository.TeacherRepository;
+//
+//import java.io.IOException;
+//import java.text.ParseException;
+//
+///**
+// * Main class where program starts.
+// */
+//public class StartApp {
+//
+//    /**
+//     * Start point of the application
+//     * @param args command line arguments
+//     */
+////    public static void main(String[] args) throws IOException, ParseException, NullPointerException, IncorrectFileNameException {
+////        TeacherRepository teacherRepository = new TeacherRepository();
+////
+////        teacherRepository.initialise();
+////
+////        CourseRepository courseRepository = new CourseRepository(teacherRepository.teachers);
+////        courseRepository.initialise();
+////
+////        StudentRepository studentRepository = new StudentRepository(courseRepository.courseRepo);
+////        try {
+////            studentRepository.initialise();
+////        } catch (Exception NullPointerException) {
+////            System.out.println("No students");
+////        }
+////
+////        courseRepository.setRelations(studentRepository);
+////        teacherRepository.setRelations(courseRepository);
+////
+////        RegistrationSystem registrationSystem = new RegistrationSystem(courseRepository,
+////                studentRepository,
+////                teacherRepository);
+////
+////        Console console = new Console(registrationSystem);
+////        console.run();
+////        registrationSystem.writeToJson();
+////    }
+//}
