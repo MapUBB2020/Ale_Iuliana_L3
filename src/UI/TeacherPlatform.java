9ab03ca2cdd9378c3eafc454f83e8e328b5d5e12
iuliana02 <iuliana02@users.noreply.github.com>
2020-12-15 13:28:03 +0200

    Just trying

--- src/UI/TeacherPlatform.java
+++ src/UI/TeacherPlatform.java
@@ -29,3 +29,38 @@ public class TeacherPlatform implements Observer {
         System.out.println("Done");
     }
 }
+
+//package UI;
+//
+//import javafx.fxml.FXML;
+//import javafx.scene.control.Label;
+//import javafx.scene.layout.AnchorPane;
+//import lab3.model.Teacher;
+//
+//import java.util.Observable;
+//import java.util.Observer;
+//
+//public class TeacherPlatform implements Observer {
+//    TeacherController teacherController;
+//    @FXML
+//    public Label IDTeacher;
+//    @FXML
+//    public Label teacherName;
+//    @FXML
+//    public Label coursesInfo;
+//    @FXML
+//    public AnchorPane mainPane;
+//
+//    public void initialize() {
+//        teacherController = new TeacherController();
+//        mainPane.getChildren().add(new Label(teacherController.teacherID.getText()));
+//        mainPane.getChildren().add(new Label(teacherController.firstName.getText()));
+//        mainPane.getChildren().add(new Label(TeacherController.coursesInfo));
+//
+//    }
+//
+//    @Override
+//    public void update(Observable o, Object arg) {
+//        mainPane.getChildren().add()
+//    }
+//}
