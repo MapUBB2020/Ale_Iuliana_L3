17de3f746432dce9e525811dd2a97e7c2b2ae2fa
iuliana02 <iuliana02@users.noreply.github.com>
2020-12-17 19:30:22 +0200

    Change the layout a little bit

--- src/UI/EnrollmentController.java
+++ src/UI/EnrollmentController.java
@@ -4,9 +4,11 @@ import Exceptions.IncorrectFileNameException;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
 import lab3.model.Course;
 import lab3.model.Student;
 
@@ -23,11 +25,25 @@ public class EnrollmentController extends Observable {
     void initialize() {
         List<Course> courses = Controller.registrationSystem.getCourseRepository().findAll();
         for (Course course: courses) {
-            enrollmentPage.getChildren().add(new Label(course.getName()));
-            enrollmentPage.getChildren().add(new Label(course.getTeacher().getFirstName()
-                    + " " + course.getTeacher().getLastName()));
-            enrollmentPage.getChildren().add(new Label(String.valueOf(course.getCredits()) + " credits"));
+            Label courseLabel = new Label("Course: " + course.getName());
+            courseLabel.setTranslateX(20);
+            courseLabel.setTranslateY(20);
+            courseLabel.setStyle("-fx-font:18 system");
+            courseLabel.setStyle("-fx-font-weight:bold ");
+            courseLabel.setPadding(new Insets(10,0,10,0));
+            enrollmentPage.getChildren().add(courseLabel);
+            Label teacherLabel = new Label("Teacher: " + course.getTeacher().getFirstName()
+                    + " " + course.getTeacher().getLastName());
+            teacherLabel.setTranslateX(20);
+            teacherLabel.setTranslateY(20);
+            enrollmentPage.getChildren().add(teacherLabel);
+            Label creditsLabel = new Label("Credits: " + String.valueOf(course.getCredits()) + " credits");
+            creditsLabel.setTranslateX(20);
+            creditsLabel.setTranslateY(20);
+            enrollmentPage.getChildren().add(creditsLabel);
             Button enrollButton = new Button();
+            enrollButton.setTranslateX(50);
+            enrollButton.setTranslateY(20);
             enrollButton.setText("ENROLL");
             addObserver(teacherPlatform);
             enrollButton.setOnAction(new EventHandler<ActionEvent>() {
@@ -43,7 +59,11 @@ public class EnrollmentController extends Observable {
                             e.printStackTrace();
                         }
                     } else {
-                        enrollmentPage.getChildren().add(new Label("Could not enroll!"));
+                        Label notEnrolledLabel = new Label("Could not enroll!");
+                        notEnrolledLabel.setTextFill(Paint.valueOf("red"));
+                        notEnrolledLabel.setStyle("-fx-font-weight: bold");
+                        notEnrolledLabel.setPadding(new Insets(60,0,0,400));
+                        enrollmentPage.getChildren().add(notEnrolledLabel);
                     }
                     enrollButton.setDisable(true);
                 }
